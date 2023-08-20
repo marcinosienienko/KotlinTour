d86d123d805c2184318e9a75ba468e1a4d11f8b6
Marcin <marcinosienienko@gmail.com>
2023-08-20 21:46:10 +0200

    "Kotlin"

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+    val popcorn = 5
+    val hotdog = 7
+    //var customers = 10
+
+    //customers = 8
+
+    val customers = 10
+    println("There are $customers customers")
+    println("There are ${customers + 1} customers")
+
+
+    val name = "Mary"
+    val age = 20
+
+    println("$name is $age years old")
+
+    var members = 10
+
+    members -= 3
+    println("Members = $members")
+
+    members += 7
+    println("Members = $members")
+
+    members *= 2
+    println("Members = $members")
+
+    members /= 4
+    println("Members = $members")
+
+}
