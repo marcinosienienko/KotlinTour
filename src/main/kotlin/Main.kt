e21a13dd0d882a9568ab6456c4ecc8e6b4ae5dce
cinek <108581387+marcinosienienko@users.noreply.github.com>
2023-09-03 00:10:56 +0200

    feat/exercise

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,42 @@
 fun main(args: Array<String>) {
 
-    val readOnlyFruit = setOf("apple", "cherry", "banana", "banana")
-    val fruit: MutableSet<String> = mutableSetOf("apple", "cherry", "banana", "banana")
-    val fruitLocked: Set<String> = fruit
+    //Read only map
+    val readOnlyJuiceMenu = mapOf("apple" to 180,"kiwi" to 250, "banana" to 300)
+    println(readOnlyJuiceMenu)
 
-    println(fruitLocked)
+    //Mutable map with explicit type declaration
+    val juiceMenu : MutableMap<String, Int> = mutableMapOf("apple" to 100, "kiwi" to 250, "banana" to 300)
+    val juiceMenuLocked: Map<String, Int> = juiceMenu
+    println(juiceMenu)
+    println(readOnlyJuiceMenu.containsKey("kiwi"))
+    println(readOnlyJuiceMenu.keys)
+    println(readOnlyJuiceMenu.values)
+    println("banana" in readOnlyJuiceMenu.keys)
+    println(200 in readOnlyJuiceMenu.values)
 
-    println("This set has ${fruit.count()} items")
+    //Exercise 1
 
-    fruit.add("dragonfruit")
-    println(fruit)
+    val greenNumbers = listOf(1,4,23)
+    val redNumbers = listOf(17,2)
 
-    fruit.remove("banana")
-    println(fruit)
+    val totalCount = greenNumbers.count() + redNumbers.count()
+
+    println(totalCount)
+
+    //Exercise 2
+
+    val SUPPORTED = setOf("HTTP", "HTTPS", "FTP")
+    val requested = "smtp"
+    val isSupported = requested.uppercase() in SUPPORTED
+
+    println("Support for $requested: $isSupported")
+
+    //Exercise 3
+
+    val number2Word = mapOf(1 to "one", 2 to "two", 3 to "three")
+        val n = 2
+    println("$n in spelt as '${number2Word[n]}'")
+
+
 }
+
